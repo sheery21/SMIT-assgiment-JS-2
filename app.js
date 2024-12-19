e881a1e6660b0cc0d7337b7b112804f2d5ddc400
shery <sherypagal123@gmail.com>
2024-12-19 19:51:54 +0500

    project added

---
+++ app.js
@@ -0,0 +1,286 @@
+// CHAPTER NO 1:
+
+// Question 1
+
+// var statement="hello world ,jhone boe,joine me no instagram"
+
+// Question 2
+
+// VARIABLE NAMES: LEGAL 
+
+// var myName = "jhone "
+// var myName1 = " doe"
+// var $name ="world"
+// var _userName = "hello world"
+
+// VARIABLE NAMES: ILLEGAL
+
+// var 1stname="jhone"
+//  var -name="world"
+// var @username="/user"
+// var total amount="100"
+// var first-name="first"
+
+// Question 3
+
+// var hading ="Rules for naming JS variables"
+// document.write("<h1>"+hading+"</h1>");
+
+// var letter =" letters ABC,  "
+// var numbers ="numbers 1, "
+// var dollarSign ="dollar sign $, "
+// var underscores =" underscores _ ."
+// document.write("<p>"+"Variable names can only contain"+letter+numbers+dollarSign+"and"+underscores +"For example"+ "<b>"+ " $my_1stVariable"+"</b>"+"</p>");
+
+// var letter =" letters ABC,  "
+// var numbers ="numbers 1, "
+// var dollarSign ="dollar sign $, "
+// var underscores =" underscores _ ."
+// document.write("<p>"+"Variables must begin with a"+letter+numbers+dollarSign+"and"+underscores +"For example"+ "<b>"+ " $name, _name "+"</b>"+"or " +"<b>"+"name"+"</b>"+"</p>");
+
+// var sensitve ="sensitve"
+// document.write ("Variable names are case "+sensitve +"<br/>"+"<br/>");
+
+// var keyword ="keyword"
+// document.write ("Variable names should not be JS "+keyword)
+
+//  CHAPTER NO:2
+
+// Question 1
+
+// var a =3
+// var b =5
+
+// document.write ("sum of 3 and 5 is ", a+b)
+
+// Question 2
+
+// var c =10
+// var b =5
+// document.write ("Addition of 10 and 5 is ", c+b  +"<br/>")
+//  var c =5
+//  var b =10
+//  document.write ("Subtraction of 10 and 5 is ", c-b+"<br/>")
+//  var c =5
+//  var b =10
+//  document.write ("Multiplication of 10 and 5 is ", c*b+"<br/>")
+//  var c =5
+//  var b =10
+//  document.write ("division of 10 and 5 is ", c/b+"<br/>")
+//  var c =5
+//  var b =10
+//  document.write ("modulus of 10 and 5 is ", c%b+"<br/>")
+ 
+// Question 3
+
+// Declare a variable
+// let myVariable;
+
+// // Show the value of the variable after declaration
+// document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+// // Initialize the variable with some number
+// myVariable = 5;
+
+// // Show the initial value
+// document.write("Initial value: " + myVariable + "<br>");
+
+// // Increment the variable
+// myVariable++;
+
+// // Show the value after increment
+// document.write("Value after increment is: " + myVariable + "<br>");
+
+// // Add 7 to the variable
+// myVariable += 7;
+
+// // Show the value after addition
+// document.write("Value after addition is: " + myVariable + "<br>");
+
+// // Decrement the variable
+// myVariable--;
+
+// // Show the value after decrement
+// document.write("Value after decrement is: " + myVariable + "<br>");
+
+// // Show the remainder after dividing the variable by 3
+// let remainder = myVariable % 3;
+// document.write("The remainder is: " + remainder);
+
+// Question 4
+
+
+// var ticket = 600
+
+// document.write ("tottol cost to buy 5 tickets to a movie is "+"<b>",ticket*5+"PKR"+"<b/>")
+
+
+// Question 5
+
+// document.write ("<b>"+"Table of 4"+"<b/>"+"<br>"+"<br>")
+// var table =4
+
+// document.write ("  4 x 1 = "+table*1  +"<br>")
+// document.write ("  4 x 2 = "+table*2  +"<br>")
+// document.write ("  4 x 3 = "+table*3  +"<br>")
+// document.write ("  4 x 4 = "+table*4  +"<br>")
+// document.write ("  4 x 5 = "+table*5  +"<br>")
+// document.write ("  4 x 6 = "+table*6  +"<br>")
+// document.write ("  4 x 7 = "+table*7  +"<br>")
+// document.write ("  4 x 8 = "+table*8  +"<br>")
+// document.write ("  4 x 9 = "+table*9  +"<br>")
+// document.write ("  4 x 10 = "+table*10  +"<br>")
+
+// Question 6
+
+
+//  var celsius = 25; 
+//  var fahrenheit = (celsius * 9/5) + 32;
+//  document.write(celsius + "°C is " + fahrenheit + "°F<br>");
+//  fahrenheit = 70; 
+//  celsius = (fahrenheit - 32) * 5/9;
+//  document.write(fahrenheit + "°F is " + celsius.toFixed(2) + "111111111111°C");
+
+// //  Question 7
+
+// var preiceOfItem1=650
+// var preiceOfItem2=100
+// var shoppingCharges =100
+
+// var tottol1 = preiceOfItem1*3
+
+// var tottol2 = preiceOfItem2*7
+
+// var totalCost = tottol1+tottol2+shoppingCharges
+
+// document.write("Total cost of buying 3 items and paying 100 shopping charges is "+"<b>",totalCost+"PKR"+"<b/>")
+
+// Question 8
+
+// document.write("<b>"+"Marks Sheet"+"</b>")
+
+// var tottalMarks=980
+
+// var studentMarks=804
+
+// var percentage= studentMarks/tottalMarks*100
+
+// document.write("<br>"+"Total Marks: 980")
+// document.write("<br>"+"Marsk obtained: 804")
+
+// document.write("<br>"+"percentage: "+percentage+"<br>")
+
+// Question 9
+
+// var usDollar =10
+//  var paks1 =104.80
+// var saudiRiyar =25
+// var paks2 =28
+
+// var exchangeRate1 = usDollar*paks1
+// var exchangeRate2 = saudiRiyar*paks2
+// var exchangeRate3 = exchangeRate1+exchangeRate2
+
+// document.write("Total currenty in PKR :" + exchangeRate3)
+
+
+// Question 10
+// var add =5
+// var by =10
+// var by1 =2
+// var result = add*by/by1
+
+// document.write("Result : " + result)
+
+// Question 11
+
+
+// var currentYear =2024
+// var birthYear =1992
+// var age =currentYear - birthYear
+// document.write("<b>"+"Age Calculator"+"</b>")
+// document.write("<br>"+"Current Year: " + currentYear)
+// document.write("<br>"+"Current Birth Year"+birthYear+"<br>")
+// document.write("Your age is : " + age)
+
+// Question 12
+
+// var circel = 20
+// var circumference =125.67999999999999
+// document.write =("<b>"+"Radus of a circle "+ circel+"<br>")
+// document.write =("<b>"+"The circumference is "+ circumference+"<br>")
+// document.write =("the area is: " +circumference-caches)
+
+// Question 13
+
+// favoriteSnack = "Chocolate Chip Cookies"
+
+// var currentAge = 15  
+
+// var maxAge = 65
+
+// var amountPerDay = 3  
+
+// var totalSnacksNeeded = (maxAge - currentAge) * 365 * amountPerDay
+
+// document.write("<b>"+"The Lifetime Sopply Calculator"+"</b>"+"<br/>")
+// document.write("Favourite Snack: Chocolate chip"+"<br/>")
+// document.write("Current age:15"+"<br/>")
+// document.write("Estimatde Maximum Age:65"+"<br/>")
+// document.write("Amount of snacks per day:3"+"<br/>")
+
+
+// document.write("You will need " + totalSnacksNeeded + " " + favoriteSnack + " to last you until the age of " + maxAge + ".")
+
+// Chapter no 3
+
+// Question 1
+
+var a =10
+// document.write("Result:"+"<br/>")
+// document.write("The value of a is 10"+"<br/>"+"<br/>")
+// document.write("The value of ++a is ", ++a +"<br/>")
+// document.write("Now the value of a is ", a +"<br/>"+"<br/>")
+// document.write("The value of a++ is ", a++ +"<br/>")
+// document.write("Now the value of a is ", a +"<br/>"+"<br/>")
+// document.write("The value of --a is ", --a +"<br/>")
+// document.write("Now the value of a is ", a +"<br/>"+"<br/>")
+// document.write("The value of a-- is ", a-- +"<br/>")
+// document.write("Now the value of a is ", a +"<br/>"+"<br/>")
+
+// Question 2
+
+// var a=2, b=1
+// var result =--a - --b  + ++b + b--
+
+// document.write("a is "+a +"<br/>")
+// document.write("b is "+b+"<br/>")
+// document.write("result is "+ result )
+
+// Question 3
+
+// Question 4/5
+
+// Question 6/7
+
+// var tottolllll =100
+// var Markssssss =54
+// var personMarks1 =54%100 
+// var tottolllll =100
+// var Markasssss =48
+// var personMarks2 =48%100 
+
+// var tottoa=tottolllll*3
+// var Markas=Markasssss+Markssssss+Markssssss
+
+// var personMarks3=Markas/tottoa*100
+
+// document.write( "<h1>"+"Subject Total Marks Obtaind Marks Percentage"+"</h1>")
+// document.write( "English "+" 100 " +"  54 "+ personMarks1+"%" +"<br/>"+"<br/>")
+// document.write( "Math "+" 100 " +"  54 "+ personMarks1+"%"+"<br/>"+"<br/>")
+// document.write( "Urdu"+" 100 " +"  54 "+ personMarks2+"%"+"<br/>"+"<br/>")
+
+// document.write( "300 "+"156 "+ personMarks3 +"%")
+
+
+
